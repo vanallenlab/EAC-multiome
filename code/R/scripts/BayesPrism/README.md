46028373b7bad40cbd8cc80560e21b96d26decad
Josephine Yates <47554333+josephineyates@users.noreply.github.com>
2024-03-18 15:25:54 +0100

    BP

--- code/R/scripts/BayesPrism/README.md
+++ code/R/scripts/BayesPrism/README.md
@@ -1,7 +1,21 @@
 # EAC-multiome
 
+This folder contains all scripts used to run BayesPrism to deconvolve the bulk data from the TCGA, Hoefnagel et al. and Carroll et al. cohorts.
+
+The information to download the RNA counts for the datasets as well as all auxiliary files are located in the [related subsections of the README](https://github.com/vanallenlab/EAC-multiome/blob/main/README.md). Data that needs to be download prior to running the scrips is indicated by the <span style="color:red">DWNL</span> tag.
+
 ## `run_BPrism.R`
 
-- `geneMap <- "/add/path/here"`
-- `path <- "add/path/here"`
-- `seuratObj <- readRDS(paste0(path, "add/path/here/sce.pub.Rds"))`
+- `geneMap <- "/add/path/here"`: this is the path to the gene mapping to map ENCODE to official gene ID <span style="color:red">DWNL</span>
+- `path <- "add/path/here"`: path to the general folder where the data is located
+- `seuratObj <- readRDS(paste0(path, "add/path/here/sce.pub.Rds"))`: path to single-cell Carroll et al. data <span style="color:red">DWNL</span>
+- `data <- read.table(paste0(path, expr), sep=',', row.names=1, header=TRUE)`: path to the bulk TCGA raw counts <span style="color:red">DWNL</span>
+- `save(bp.res, file=paste0(path, "/add/path/here/eac.bp.res.RData"))`: path to where the results should be saved
+
+The information can easily be extracted from the `.RData` object using the following commands
+```
+# to obtain the proportions of each cell type
+theta <- get.fraction (bp=bp.res, which.theta="final",
+             state.or.type="type")
+```
+ 
--- code/R/scripts/BayesPrism/run_BPrism_Carroll.R
+++ code/R/scripts/BayesPrism/run_BPrism_Carroll.R
@@ -1,41 +1,30 @@
 library(BayesPrism)
 library(Seurat)
-#library(SeuratDisk)
 
-#source TCGA: https://xenabrowser.net/datapages/?dataset=TCGA-ESCA.htseq_counts.tsv&host=https%3A%2F%2Fgdc.xenahubs.net&removeHub=https%3A%2F%2Fxena.treehouse.gi.ucsc.edu%3A443
+path <- "/add/path/here"
 
-geneMap <- "/add/path/here"
-path <- "add/path/here"
-
-expr <- "auxiliary_data/tcga/TCGA-ESCA.htseq_counts.tsv"
+expr <- "/add/path/here/GSE207526_110.EAC.and.10.Normal.for.GSEA.txt"
 
 seuratObj <- readRDS(paste0(path, "add/path/here/sce.pub.Rds"))
 
-tcga <- read.table(paste0(path, expr), sep='\t', row.names=1, header=TRUE) #it is log2(count+1)
-
-tcga <- (2^tcga) -1
-
+data <- read.table(paste0(path, expr), sep='\t', row.names=1, header=TRUE) 
 
 #prepare data for bayesprism:
 
-bk.dat = t(tcga[1:60483,]) # to omit info about mapped reads etc
+bk.dat <- t(data)
+bk.dat <- bk.dat[,2:18845]
 
 ### EAC
 celltypes = seuratObj$subcompartment
 cellstates = seuratObj$cellsubtype
-
 idx_escc <- grep("ESCC-", cellstates)
 idx_eac <- grep("EAC-", cellstates)
-celltypes[c(idx_eac,idx_escc)] <- "tumor"
 cellstates[c(idx_eac)] <- "EAC"
 cellstates[c(idx_escc)] <- "ESCC"
+celltypes[c(idx_eac,idx_escc)] <- "tumor"
 
+### EAC
 sc.dat <- as.matrix(t(seuratObj@assays$RNA@counts))
-
-gene2id <- read.table(paste0(path, geneMap), sep='\t', row.names=1, header=TRUE)
-
-colnames(bk.dat) <- gene2id[colnames(bk.dat),"gene"] #to make sure bk and sc data have the same gene names
-
 bk.dat <- bk.dat[,!duplicated(colnames(bk.dat))] #remove duplicated genes
 
 #create BP object:
@@ -44,12 +33,11 @@ myPrism <- new.prism(reference=sc.dat, mixture=bk.dat, input.type="count.matrix"
                      cell.type.labels = celltypes, cell.state.labels = cellstates,
                      key="tumor", outlier.cut = 0.001, outlier.fraction=0.1)
 
-
 #run BP:
 
 bp.res <- run.prism(prism=myPrism, n.cores=15, update.gibbs = TRUE)
 
 
 #save the object for later:
-save(bp.res, file=paste0(path, "/add/path/here/eac.bp.res.RData"))
+save(bp.res, file=paste0(path, "/add/path/here/eac-gse207526.bp.res.RData"))
 
